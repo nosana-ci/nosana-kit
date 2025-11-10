fd0bcf24fe458066ca8d688c88d9d1dd33207ed8
Laurens Verspeek <l.verspeek@gmail.com>
2025-11-10 20:10:09 +0100

    fix formatting

--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,11 @@ export type { Stake } from './programs/StakeProgram.js';
 
 // Export MerkleDistributorProgram and related types
 export { MerkleDistributorProgram } from './programs/MerkleDistributorProgram.js';
-export type { MerkleDistributor, ClaimStatus, ClaimTarget } from './programs/MerkleDistributorProgram.js';
+export type {
+  MerkleDistributor,
+  ClaimStatus,
+  ClaimTarget,
+} from './programs/MerkleDistributorProgram.js';
 
 // Export IPFS utilities
 export * from './ipfs/IPFS.js';
